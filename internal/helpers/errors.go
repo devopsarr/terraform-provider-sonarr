aa7f7fa5c979ef343e310578d8c3a9f43afdef77
Fuochi <ffuochi@hotmail.com>
2022-09-27 15:02:06 +0400

    refactor: move errors to helpers

---
+++ internal/helpers/errors.go
@@ -0,0 +1,30 @@
+package helpers
+
+import (
+	"errors"
+	"fmt"
+)
+
+// define constant for error management.
+const (
+	ClientError                       = "Client Error"
+	ResourceError                     = "Resource Error"
+	DataSourceError                   = "Data Source Error"
+	UnexpectedImportIdentifier        = "Unexpected Import Identifier"
+	UnexpectedResourceConfigureType   = "Unexpected Resource Configure Type"
+	UnexpectedDataSourceConfigureType = "Unexpected DataSource Configure Type"
+)
+
+var ErrDataNotFound = errors.New("data source not found")
+
+func ErrDataNotFoundError(kind, field, search string) error {
+	return fmt.Errorf("%w: no %s with %s '%s'", ErrDataNotFound, kind, field, search)
+}
+
+func UnableToRead(name string, err error) string {
+	return fmt.Sprintf("Unable to read %s, got error: %s", name, err)
+}
+
+func WrongSonarrClient(providerData interface{}) string {
+	return fmt.Sprintf("Expected *sonarr.Sonarr, got: %T. Please report this issue to the provider developers.", providerData)
+}
