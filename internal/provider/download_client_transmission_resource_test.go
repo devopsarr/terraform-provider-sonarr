07c14f313f62103a5825dad824014913f7b1ea68
Fuochi <ffuochi@hotmail.com>
2022-11-02 11:12:21 +0400

    feat: add download client transmission

---
+++ internal/provider/download_client_transmission_resource_test.go
@@ -0,0 +1,54 @@
+package provider
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+)
+
+func TestAccDownloadClientTransmissionResource(t *testing.T) {
+	t.Parallel()
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:                 func() { testAccPreCheck(t) },
+		ProtoV6ProviderFactories: testAccProtoV6ProviderFactories,
+		Steps: []resource.TestStep{
+			// Create and Read testing
+			{
+				Config: testAccDownloadClientTransmissionResourceConfig("resourceTransmissionTest", "false"),
+				Check: resource.ComposeAggregateTestCheckFunc(
+					resource.TestCheckResourceAttr("sonarr_download_client_transmission.test", "enable", "false"),
+					resource.TestCheckResourceAttr("sonarr_download_client_transmission.test", "url_base", "/transmission/"),
+					resource.TestCheckResourceAttrSet("sonarr_download_client_transmission.test", "id"),
+				),
+			},
+			// Update and Read testing
+			{
+				Config: testAccDownloadClientTransmissionResourceConfig("resourceTransmissionTest", "true"),
+				Check: resource.ComposeAggregateTestCheckFunc(
+					resource.TestCheckResourceAttr("sonarr_download_client_transmission.test", "enable", "true"),
+				),
+			},
+			// ImportState testing
+			{
+				ResourceName:      "sonarr_download_client_transmission.test",
+				ImportState:       true,
+				ImportStateVerify: true,
+			},
+			// Delete testing automatically occurs in TestCase
+		},
+	})
+}
+
+func testAccDownloadClientTransmissionResourceConfig(name, enable string) string {
+	return fmt.Sprintf(`
+	resource "sonarr_download_client_transmission" "test" {
+		enable = %s
+		priority = 1
+		name = "%s"
+		host = "transmission"
+		url_base = "/transmission/"
+		port = 9091
+	}`, enable, name)
+}
