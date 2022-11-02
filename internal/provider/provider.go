bbdc2544f093389df19e59704371077be5323795
Fuochi <ffuochi@hotmail.com>
2022-11-02 15:03:20 +0400

    feat: add release profiles datasource

--- internal/provider/provider.go
+++ internal/provider/provider.go
@@ -172,6 +172,7 @@ func (p *SonarrProvider) DataSources(ctx context.Context) []func() datasource.Da
 		NewNotificationsDataSource,
 		NewQualityProfileDataSource,
 		NewQualityProfilesDataSource,
+		NewReleaseProfilesDataSource,
 		NewRemotePathMappingDataSource,
 		NewRemotePathMappingsDataSource,
 		NewRootFolderDataSource,
