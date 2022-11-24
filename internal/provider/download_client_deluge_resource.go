158c17b2bb4a6dd6d6bb800039ae17f477469a51
Fuochi <ffuochi@hotmail.com>
2022-11-24 15:18:40 +0400

    feat: add download client deluge resource

---
+++ internal/provider/download_client_deluge_resource.go
@@ -0,0 +1,408 @@
+package provider
+
+import (
+	"context"
+	"fmt"
+	"strconv"
+
+	"github.com/devopsarr/terraform-provider-sonarr/tools"
+	"github.com/hashicorp/terraform-plugin-framework/diag"
+	"github.com/hashicorp/terraform-plugin-framework/path"
+	"github.com/hashicorp/terraform-plugin-framework/resource"
+	"github.com/hashicorp/terraform-plugin-framework/tfsdk"
+	"github.com/hashicorp/terraform-plugin-framework/types"
+	"github.com/hashicorp/terraform-plugin-log/tflog"
+	"golift.io/starr/sonarr"
+)
+
+const (
+	downloadClientDelugeResourceName   = "download_client_deluge"
+	DownloadClientDelugeImplementation = "Deluge"
+	DownloadClientDelugeConfigContrat  = "DelugeSettings"
+	DownloadClientDelugeProtocol       = "torrent"
+)
+
+// Ensure provider defined types fully satisfy framework interfaces.
+var _ resource.Resource = &DownloadClientDelugeResource{}
+var _ resource.ResourceWithImportState = &DownloadClientDelugeResource{}
+
+func NewDownloadClientDelugeResource() resource.Resource {
+	return &DownloadClientDelugeResource{}
+}
+
+// DownloadClientDelugeResource defines the download client implementation.
+type DownloadClientDelugeResource struct {
+	client *sonarr.Sonarr
+}
+
+// DownloadClientDeluge describes the download client data model.
+type DownloadClientDeluge struct {
+	Tags                     types.Set    `tfsdk:"tags"`
+	Name                     types.String `tfsdk:"name"`
+	Host                     types.String `tfsdk:"host"`
+	URLBase                  types.String `tfsdk:"url_base"`
+	Username                 types.String `tfsdk:"username"`
+	Password                 types.String `tfsdk:"password"`
+	TvCategory               types.String `tfsdk:"tv_category"`
+	TvDirectory              types.String `tfsdk:"tv_directory"`
+	TvImportedCategory       types.String `tfsdk:"tv_imported_category"`
+	RecentTvPriority         types.Int64  `tfsdk:"recent_tv_priority"`
+	OlderTvPriority          types.Int64  `tfsdk:"older_tv_priority"`
+	Priority                 types.Int64  `tfsdk:"priority"`
+	Port                     types.Int64  `tfsdk:"port"`
+	ID                       types.Int64  `tfsdk:"id"`
+	AddPaused                types.Bool   `tfsdk:"add_paused"`
+	UseSsl                   types.Bool   `tfsdk:"use_ssl"`
+	Enable                   types.Bool   `tfsdk:"enable"`
+	RemoveFailedDownloads    types.Bool   `tfsdk:"remove_failed_downloads"`
+	RemoveCompletedDownloads types.Bool   `tfsdk:"remove_completed_downloads"`
+}
+
+func (d DownloadClientDeluge) toDownloadClient() *DownloadClient {
+	return &DownloadClient{
+		Tags:                     d.Tags,
+		Name:                     d.Name,
+		Host:                     d.Host,
+		URLBase:                  d.URLBase,
+		Username:                 d.Username,
+		Password:                 d.Password,
+		TvCategory:               d.TvCategory,
+		TvDirectory:              d.TvDirectory,
+		TvImportedCategory:       d.TvImportedCategory,
+		RecentTvPriority:         d.RecentTvPriority,
+		OlderTvPriority:          d.OlderTvPriority,
+		Priority:                 d.Priority,
+		Port:                     d.Port,
+		ID:                       d.ID,
+		AddPaused:                d.AddPaused,
+		UseSsl:                   d.UseSsl,
+		Enable:                   d.Enable,
+		RemoveFailedDownloads:    d.RemoveFailedDownloads,
+		RemoveCompletedDownloads: d.RemoveCompletedDownloads,
+	}
+}
+
+func (d *DownloadClientDeluge) fromDownloadClient(client *DownloadClient) {
+	d.Tags = client.Tags
+	d.Name = client.Name
+	d.Host = client.Host
+	d.URLBase = client.URLBase
+	d.Username = client.Username
+	d.Password = client.Password
+	d.TvCategory = client.TvCategory
+	d.TvDirectory = client.TvDirectory
+	d.TvImportedCategory = client.TvImportedCategory
+	d.RecentTvPriority = client.RecentTvPriority
+	d.OlderTvPriority = client.OlderTvPriority
+	d.Priority = client.Priority
+	d.Port = client.Port
+	d.ID = client.ID
+	d.AddPaused = client.AddPaused
+	d.UseSsl = client.UseSsl
+	d.Enable = client.Enable
+	d.RemoveFailedDownloads = client.RemoveFailedDownloads
+	d.RemoveCompletedDownloads = client.RemoveCompletedDownloads
+}
+
+func (r *DownloadClientDelugeResource) Metadata(ctx context.Context, req resource.MetadataRequest, resp *resource.MetadataResponse) {
+	resp.TypeName = req.ProviderTypeName + "_" + downloadClientDelugeResourceName
+}
+
+func (r *DownloadClientDelugeResource) GetSchema(ctx context.Context) (tfsdk.Schema, diag.Diagnostics) {
+	return tfsdk.Schema{
+		MarkdownDescription: "<!-- subcategory:Download Clients -->Download Client Deluge resource.\nFor more information refer to [Download Client](https://wiki.servarr.com/sonarr/settings#download-clients) and [Deluge](https://wiki.servarr.com/sonarr/supported#deluge).",
+		Attributes: map[string]tfsdk.Attribute{
+			"enable": {
+				MarkdownDescription: "Enable flag.",
+				Optional:            true,
+				Computed:            true,
+				Type:                types.BoolType,
+			},
+			"remove_completed_downloads": {
+				MarkdownDescription: "Remove completed downloads flag.",
+				Optional:            true,
+				Computed:            true,
+				Type:                types.BoolType,
+			},
+			"remove_failed_downloads": {
+				MarkdownDescription: "Remove failed downloads flag.",
+				Optional:            true,
+				Computed:            true,
+				Type:                types.BoolType,
+			},
+			"priority": {
+				MarkdownDescription: "Priority.",
+				Optional:            true,
+				Computed:            true,
+				Type:                types.Int64Type,
+			},
+			"name": {
+				MarkdownDescription: "Download Client name.",
+				Required:            true,
+				Type:                types.StringType,
+			},
+			"tags": {
+				MarkdownDescription: "List of associated tags.",
+				Optional:            true,
+				Computed:            true,
+				Type: types.SetType{
+					ElemType: types.Int64Type,
+				},
+			},
+			"id": {
+				MarkdownDescription: "Download Client ID.",
+				Computed:            true,
+				Type:                types.Int64Type,
+				PlanModifiers: tfsdk.AttributePlanModifiers{
+					resource.UseStateForUnknown(),
+				},
+			},
+			// Field values
+			"add_paused": {
+				MarkdownDescription: "Add paused flag.",
+				Optional:            true,
+				Computed:            true,
+				Type:                types.BoolType,
+			},
+			"use_ssl": {
+				MarkdownDescription: "Use SSL flag.",
+				Optional:            true,
+				Computed:            true,
+				Type:                types.BoolType,
+			},
+			"port": {
+				MarkdownDescription: "Port.",
+				Optional:            true,
+				Computed:            true,
+				Type:                types.Int64Type,
+			},
+			"recent_tv_priority": {
+				MarkdownDescription: "Recent TV priority. `0` Last, `1` First.",
+				Optional:            true,
+				Computed:            true,
+				Type:                types.Int64Type,
+				Validators: []tfsdk.AttributeValidator{
+					tools.IntMatch([]int64{0, 1}),
+				},
+			},
+			"older_tv_priority": {
+				MarkdownDescription: "Older TV priority. `0` Last, `1` First.",
+				Optional:            true,
+				Computed:            true,
+				Type:                types.Int64Type,
+				Validators: []tfsdk.AttributeValidator{
+					tools.IntMatch([]int64{0, 1}),
+				},
+			},
+			"host": {
+				MarkdownDescription: "host.",
+				Optional:            true,
+				Computed:            true,
+				Type:                types.StringType,
+			},
+			"url_base": {
+				MarkdownDescription: "Base URL.",
+				Optional:            true,
+				Computed:            true,
+				Type:                types.StringType,
+			},
+			"username": {
+				MarkdownDescription: "Username.",
+				Optional:            true,
+				Computed:            true,
+				Type:                types.StringType,
+			},
+			"password": {
+				MarkdownDescription: "Password.",
+				Optional:            true,
+				Computed:            true,
+				Type:                types.StringType,
+			},
+			"tv_category": {
+				MarkdownDescription: "TV category.",
+				Optional:            true,
+				Computed:            true,
+				Type:                types.StringType,
+			},
+			"tv_directory": {
+				MarkdownDescription: "TV directory.",
+				Optional:            true,
+				Computed:            true,
+				Type:                types.StringType,
+			},
+			"tv_imported_category": {
+				MarkdownDescription: "TV imported category.",
+				Optional:            true,
+				Computed:            true,
+				Type:                types.StringType,
+			},
+		},
+	}, nil
+}
+
+func (r *DownloadClientDelugeResource) Configure(ctx context.Context, req resource.ConfigureRequest, resp *resource.ConfigureResponse) {
+	// Prevent panic if the provider has not been configured.
+	if req.ProviderData == nil {
+		return
+	}
+
+	client, ok := req.ProviderData.(*sonarr.Sonarr)
+	if !ok {
+		resp.Diagnostics.AddError(
+			tools.UnexpectedResourceConfigureType,
+			fmt.Sprintf("Expected *sonarr.Sonarr, got: %T. Please report this issue to the provider developers.", req.ProviderData),
+		)
+
+		return
+	}
+
+	r.client = client
+}
+
+func (r *DownloadClientDelugeResource) Create(ctx context.Context, req resource.CreateRequest, resp *resource.CreateResponse) {
+	// Retrieve values from plan
+	var client *DownloadClientDeluge
+
+	resp.Diagnostics.Append(req.Plan.Get(ctx, &client)...)
+
+	if resp.Diagnostics.HasError() {
+		return
+	}
+
+	// Create new DownloadClientDeluge
+	request := client.read(ctx)
+
+	response, err := r.client.AddDownloadClientContext(ctx, request)
+	if err != nil {
+		resp.Diagnostics.AddError(tools.ClientError, fmt.Sprintf("Unable to create %s, got error: %s", downloadClientDelugeResourceName, err))
+
+		return
+	}
+
+	tflog.Trace(ctx, "created "+downloadClientDelugeResourceName+": "+strconv.Itoa(int(response.ID)))
+	// Generate resource state struct
+	client.write(ctx, response)
+	resp.Diagnostics.Append(resp.State.Set(ctx, &client)...)
+}
+
+func (r *DownloadClientDelugeResource) Read(ctx context.Context, req resource.ReadRequest, resp *resource.ReadResponse) {
+	// Get current state
+	var client DownloadClientDeluge
+
+	resp.Diagnostics.Append(req.State.Get(ctx, &client)...)
+
+	if resp.Diagnostics.HasError() {
+		return
+	}
+
+	// Get DownloadClientDeluge current value
+	response, err := r.client.GetDownloadClientContext(ctx, client.ID.ValueInt64())
+	if err != nil {
+		resp.Diagnostics.AddError(tools.ClientError, fmt.Sprintf("Unable to read %s, got error: %s", downloadClientDelugeResourceName, err))
+
+		return
+	}
+
+	tflog.Trace(ctx, "read "+downloadClientDelugeResourceName+": "+strconv.Itoa(int(response.ID)))
+	// Map response body to resource schema attribute
+	client.write(ctx, response)
+	resp.Diagnostics.Append(resp.State.Set(ctx, &client)...)
+}
+
+func (r *DownloadClientDelugeResource) Update(ctx context.Context, req resource.UpdateRequest, resp *resource.UpdateResponse) {
+	// Get plan values
+	var client *DownloadClientDeluge
+
+	resp.Diagnostics.Append(req.Plan.Get(ctx, &client)...)
+
+	if resp.Diagnostics.HasError() {
+		return
+	}
+
+	// Update DownloadClientDeluge
+	request := client.read(ctx)
+
+	response, err := r.client.UpdateDownloadClientContext(ctx, request)
+	if err != nil {
+		resp.Diagnostics.AddError(tools.ClientError, fmt.Sprintf("Unable to update %s, got error: %s", downloadClientDelugeResourceName, err))
+
+		return
+	}
+
+	tflog.Trace(ctx, "updated "+downloadClientDelugeResourceName+": "+strconv.Itoa(int(response.ID)))
+	// Generate resource state struct
+	client.write(ctx, response)
+	resp.Diagnostics.Append(resp.State.Set(ctx, &client)...)
+}
+
+func (r *DownloadClientDelugeResource) Delete(ctx context.Context, req resource.DeleteRequest, resp *resource.DeleteResponse) {
+	var client *DownloadClientDeluge
+
+	resp.Diagnostics.Append(req.State.Get(ctx, &client)...)
+
+	if resp.Diagnostics.HasError() {
+		return
+	}
+
+	// Delete DownloadClientDeluge current value
+	err := r.client.DeleteDownloadClientContext(ctx, client.ID.ValueInt64())
+	if err != nil {
+		resp.Diagnostics.AddError(tools.ClientError, fmt.Sprintf("Unable to read %s, got error: %s", downloadClientDelugeResourceName, err))
+
+		return
+	}
+
+	tflog.Trace(ctx, "deleted "+downloadClientDelugeResourceName+": "+strconv.Itoa(int(client.ID.ValueInt64())))
+	resp.State.RemoveResource(ctx)
+}
+
+func (r *DownloadClientDelugeResource) ImportState(ctx context.Context, req resource.ImportStateRequest, resp *resource.ImportStateResponse) {
+	// resource.ImportStatePassthroughID(ctx, path.Root("id"), req, resp)
+	id, err := strconv.Atoi(req.ID)
+	if err != nil {
+		resp.Diagnostics.AddError(
+			tools.UnexpectedImportIdentifier,
+			fmt.Sprintf("Expected import identifier with format: ID. Got: %q", req.ID),
+		)
+
+		return
+	}
+
+	tflog.Trace(ctx, "imported "+downloadClientDelugeResourceName+": "+strconv.Itoa(id))
+	resp.Diagnostics.Append(resp.State.SetAttribute(ctx, path.Root("id"), id)...)
+}
+
+func (d *DownloadClientDeluge) write(ctx context.Context, downloadClient *sonarr.DownloadClientOutput) {
+	genericDownloadClient := DownloadClient{
+		Enable:                   types.BoolValue(downloadClient.Enable),
+		RemoveCompletedDownloads: types.BoolValue(downloadClient.RemoveCompletedDownloads),
+		RemoveFailedDownloads:    types.BoolValue(downloadClient.RemoveFailedDownloads),
+		Priority:                 types.Int64Value(int64(downloadClient.Priority)),
+		ID:                       types.Int64Value(downloadClient.ID),
+		Name:                     types.StringValue(downloadClient.Name),
+		Tags:                     types.SetValueMust(types.Int64Type, nil),
+	}
+	tfsdk.ValueFrom(ctx, downloadClient.Tags, genericDownloadClient.Tags.Type(ctx), &genericDownloadClient.Tags)
+	genericDownloadClient.writeFields(ctx, downloadClient.Fields)
+	d.fromDownloadClient(&genericDownloadClient)
+}
+
+func (d *DownloadClientDeluge) read(ctx context.Context) *sonarr.DownloadClientInput {
+	var tags []int
+
+	tfsdk.ValueAs(ctx, d.Tags, &tags)
+
+	return &sonarr.DownloadClientInput{
+		Enable:                   d.Enable.ValueBool(),
+		RemoveCompletedDownloads: d.RemoveCompletedDownloads.ValueBool(),
+		RemoveFailedDownloads:    d.RemoveFailedDownloads.ValueBool(),
+		Priority:                 int(d.Priority.ValueInt64()),
+		ID:                       d.ID.ValueInt64(),
+		ConfigContract:           DownloadClientDelugeConfigContrat,
+		Implementation:           DownloadClientDelugeImplementation,
+		Name:                     d.Name.ValueString(),
+		Protocol:                 DownloadClientDelugeProtocol,
+		Tags:                     tags,
+		Fields:                   d.toDownloadClient().readFields(ctx),
+	}
+}
---
+++ internal/provider/download_client_deluge_resource_test.go
@@ -0,0 +1,54 @@
+package provider
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+)
+
+func TestAccDownloadClientDelugeResource(t *testing.T) {
+	t.Parallel()
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:                 func() { testAccPreCheck(t) },
+		ProtoV6ProviderFactories: testAccProtoV6ProviderFactories,
+		Steps: []resource.TestStep{
+			// Create and Read testing
+			{
+				Config: testAccDownloadClientDelugeResourceConfig("resourceDelugeTest", "deluge"),
+				Check: resource.ComposeAggregateTestCheckFunc(
+					resource.TestCheckResourceAttr("sonarr_download_client_deluge.test", "host", "deluge"),
+					resource.TestCheckResourceAttr("sonarr_download_client_deluge.test", "url_base", "/deluge/"),
+					resource.TestCheckResourceAttrSet("sonarr_download_client_deluge.test", "id"),
+				),
+			},
+			// Update and Read testing
+			{
+				Config: testAccDownloadClientDelugeResourceConfig("resourceDelugeTest", "deluge-host"),
+				Check: resource.ComposeAggregateTestCheckFunc(
+					resource.TestCheckResourceAttr("sonarr_download_client_deluge.test", "host", "deluge-host"),
+				),
+			},
+			// ImportState testing
+			{
+				ResourceName:      "sonarr_download_client_deluge.test",
+				ImportState:       true,
+				ImportStateVerify: true,
+			},
+			// Delete testing automatically occurs in TestCase
+		},
+	})
+}
+
+func testAccDownloadClientDelugeResourceConfig(name, host string) string {
+	return fmt.Sprintf(`
+	resource "sonarr_download_client_deluge" "test" {
+		enable = false
+		priority = 1
+		name = "%s"
+		host = "%s"
+		url_base = "/deluge/"
+		port = 9091
+	}`, name, host)
+}
