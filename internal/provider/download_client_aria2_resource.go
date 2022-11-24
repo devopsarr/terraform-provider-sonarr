bed4a61938af886d0cd8b875729cff7501b7cecb
Fuochi <ffuochi@hotmail.com>
2022-11-24 15:27:38 +0400

    feat: add download client aria2 resource

---
+++ internal/provider/download_client_aria2_resource.go
@@ -0,0 +1,339 @@
+package provider
+
+import (
+	"context"
+	"fmt"
+	"strconv"
+
+	"github.com/devopsarr/terraform-provider-sonarr/tools"
+	"github.com/hashicorp/terraform-plugin-framework/diag"
+	"github.com/hashicorp/terraform-plugin-framework/path"
+	"github.com/hashicorp/terraform-plugin-framework/resource"
+	"github.com/hashicorp/terraform-plugin-framework/tfsdk"
+	"github.com/hashicorp/terraform-plugin-framework/types"
+	"github.com/hashicorp/terraform-plugin-log/tflog"
+	"golift.io/starr/sonarr"
+)
+
+const (
+	downloadClientAria2ResourceName   = "download_client_aria2"
+	DownloadClientAria2Implementation = "Aria2"
+	DownloadClientAria2ConfigContrat  = "Aria2Settings"
+	DownloadClientAria2Protocol       = "torrent"
+)
+
+// Ensure provider defined types fully satisfy framework interfaces.
+var _ resource.Resource = &DownloadClientAria2Resource{}
+var _ resource.ResourceWithImportState = &DownloadClientAria2Resource{}
+
+func NewDownloadClientAria2Resource() resource.Resource {
+	return &DownloadClientAria2Resource{}
+}
+
+// DownloadClientAria2Resource defines the download client implementation.
+type DownloadClientAria2Resource struct {
+	client *sonarr.Sonarr
+}
+
+// DownloadClientAria2 describes the download client data model.
+type DownloadClientAria2 struct {
+	Tags                     types.Set    `tfsdk:"tags"`
+	Name                     types.String `tfsdk:"name"`
+	Host                     types.String `tfsdk:"host"`
+	RPCPath                  types.String `tfsdk:"rpc_path"`
+	SecretToken              types.String `tfsdk:"secret_token"`
+	Priority                 types.Int64  `tfsdk:"priority"`
+	Port                     types.Int64  `tfsdk:"port"`
+	ID                       types.Int64  `tfsdk:"id"`
+	UseSsl                   types.Bool   `tfsdk:"use_ssl"`
+	Enable                   types.Bool   `tfsdk:"enable"`
+	RemoveFailedDownloads    types.Bool   `tfsdk:"remove_failed_downloads"`
+	RemoveCompletedDownloads types.Bool   `tfsdk:"remove_completed_downloads"`
+}
+
+func (d DownloadClientAria2) toDownloadClient() *DownloadClient {
+	return &DownloadClient{
+		Tags:                     d.Tags,
+		Name:                     d.Name,
+		Host:                     d.Host,
+		RPCPath:                  d.RPCPath,
+		SecretToken:              d.SecretToken,
+		Priority:                 d.Priority,
+		Port:                     d.Port,
+		ID:                       d.ID,
+		UseSsl:                   d.UseSsl,
+		Enable:                   d.Enable,
+		RemoveFailedDownloads:    d.RemoveFailedDownloads,
+		RemoveCompletedDownloads: d.RemoveCompletedDownloads,
+	}
+}
+
+func (d *DownloadClientAria2) fromDownloadClient(client *DownloadClient) {
+	d.Tags = client.Tags
+	d.Name = client.Name
+	d.Host = client.Host
+	d.RPCPath = client.RPCPath
+	d.SecretToken = client.SecretToken
+	d.Priority = client.Priority
+	d.Port = client.Port
+	d.ID = client.ID
+	d.UseSsl = client.UseSsl
+	d.Enable = client.Enable
+	d.RemoveFailedDownloads = client.RemoveFailedDownloads
+	d.RemoveCompletedDownloads = client.RemoveCompletedDownloads
+}
+
+func (r *DownloadClientAria2Resource) Metadata(ctx context.Context, req resource.MetadataRequest, resp *resource.MetadataResponse) {
+	resp.TypeName = req.ProviderTypeName + "_" + downloadClientAria2ResourceName
+}
+
+func (r *DownloadClientAria2Resource) GetSchema(ctx context.Context) (tfsdk.Schema, diag.Diagnostics) {
+	return tfsdk.Schema{
+		MarkdownDescription: "<!-- subcategory:Download Clients -->Download Client Aria2 resource.\nFor more information refer to [Download Client](https://wiki.servarr.com/sonarr/settings#download-clients) and [Aria2](https://wiki.servarr.com/sonarr/supported#aria2).",
+		Attributes: map[string]tfsdk.Attribute{
+			"enable": {
+				MarkdownDescription: "Enable flag.",
+				Optional:            true,
+				Computed:            true,
+				Type:                types.BoolType,
+			},
+			"remove_completed_downloads": {
+				MarkdownDescription: "Remove completed downloads flag.",
+				Optional:            true,
+				Computed:            true,
+				Type:                types.BoolType,
+			},
+			"remove_failed_downloads": {
+				MarkdownDescription: "Remove failed downloads flag.",
+				Optional:            true,
+				Computed:            true,
+				Type:                types.BoolType,
+			},
+			"priority": {
+				MarkdownDescription: "Priority.",
+				Optional:            true,
+				Computed:            true,
+				Type:                types.Int64Type,
+			},
+			"name": {
+				MarkdownDescription: "Download Client name.",
+				Required:            true,
+				Type:                types.StringType,
+			},
+			"tags": {
+				MarkdownDescription: "List of associated tags.",
+				Optional:            true,
+				Computed:            true,
+				Type: types.SetType{
+					ElemType: types.Int64Type,
+				},
+			},
+			"id": {
+				MarkdownDescription: "Download Client ID.",
+				Computed:            true,
+				Type:                types.Int64Type,
+				PlanModifiers: tfsdk.AttributePlanModifiers{
+					resource.UseStateForUnknown(),
+				},
+			},
+			// Field values
+			"use_ssl": {
+				MarkdownDescription: "Use SSL flag.",
+				Optional:            true,
+				Computed:            true,
+				Type:                types.BoolType,
+			},
+			"port": {
+				MarkdownDescription: "Port.",
+				Optional:            true,
+				Computed:            true,
+				Type:                types.Int64Type,
+			},
+			"host": {
+				MarkdownDescription: "host.",
+				Optional:            true,
+				Computed:            true,
+				Type:                types.StringType,
+			},
+			"rpc_path": {
+				MarkdownDescription: "RPC path.",
+				Optional:            true,
+				Computed:            true,
+				Type:                types.StringType,
+			},
+			"secret_token": {
+				MarkdownDescription: "Secret token.",
+				Optional:            true,
+				Computed:            true,
+				Type:                types.StringType,
+			},
+		},
+	}, nil
+}
+
+func (r *DownloadClientAria2Resource) Configure(ctx context.Context, req resource.ConfigureRequest, resp *resource.ConfigureResponse) {
+	// Prevent panic if the provider has not been configured.
+	if req.ProviderData == nil {
+		return
+	}
+
+	client, ok := req.ProviderData.(*sonarr.Sonarr)
+	if !ok {
+		resp.Diagnostics.AddError(
+			tools.UnexpectedResourceConfigureType,
+			fmt.Sprintf("Expected *sonarr.Sonarr, got: %T. Please report this issue to the provider developers.", req.ProviderData),
+		)
+
+		return
+	}
+
+	r.client = client
+}
+
+func (r *DownloadClientAria2Resource) Create(ctx context.Context, req resource.CreateRequest, resp *resource.CreateResponse) {
+	// Retrieve values from plan
+	var client *DownloadClientAria2
+
+	resp.Diagnostics.Append(req.Plan.Get(ctx, &client)...)
+
+	if resp.Diagnostics.HasError() {
+		return
+	}
+
+	// Create new DownloadClientAria2
+	request := client.read(ctx)
+
+	response, err := r.client.AddDownloadClientContext(ctx, request)
+	if err != nil {
+		resp.Diagnostics.AddError(tools.ClientError, fmt.Sprintf("Unable to create %s, got error: %s", downloadClientAria2ResourceName, err))
+
+		return
+	}
+
+	tflog.Trace(ctx, "created "+downloadClientAria2ResourceName+": "+strconv.Itoa(int(response.ID)))
+	// Generate resource state struct
+	client.write(ctx, response)
+	resp.Diagnostics.Append(resp.State.Set(ctx, &client)...)
+}
+
+func (r *DownloadClientAria2Resource) Read(ctx context.Context, req resource.ReadRequest, resp *resource.ReadResponse) {
+	// Get current state
+	var client DownloadClientAria2
+
+	resp.Diagnostics.Append(req.State.Get(ctx, &client)...)
+
+	if resp.Diagnostics.HasError() {
+		return
+	}
+
+	// Get DownloadClientAria2 current value
+	response, err := r.client.GetDownloadClientContext(ctx, client.ID.ValueInt64())
+	if err != nil {
+		resp.Diagnostics.AddError(tools.ClientError, fmt.Sprintf("Unable to read %s, got error: %s", downloadClientAria2ResourceName, err))
+
+		return
+	}
+
+	tflog.Trace(ctx, "read "+downloadClientAria2ResourceName+": "+strconv.Itoa(int(response.ID)))
+	// Map response body to resource schema attribute
+	client.write(ctx, response)
+	resp.Diagnostics.Append(resp.State.Set(ctx, &client)...)
+}
+
+func (r *DownloadClientAria2Resource) Update(ctx context.Context, req resource.UpdateRequest, resp *resource.UpdateResponse) {
+	// Get plan values
+	var client *DownloadClientAria2
+
+	resp.Diagnostics.Append(req.Plan.Get(ctx, &client)...)
+
+	if resp.Diagnostics.HasError() {
+		return
+	}
+
+	// Update DownloadClientAria2
+	request := client.read(ctx)
+
+	response, err := r.client.UpdateDownloadClientContext(ctx, request)
+	if err != nil {
+		resp.Diagnostics.AddError(tools.ClientError, fmt.Sprintf("Unable to update %s, got error: %s", downloadClientAria2ResourceName, err))
+
+		return
+	}
+
+	tflog.Trace(ctx, "updated "+downloadClientAria2ResourceName+": "+strconv.Itoa(int(response.ID)))
+	// Generate resource state struct
+	client.write(ctx, response)
+	resp.Diagnostics.Append(resp.State.Set(ctx, &client)...)
+}
+
+func (r *DownloadClientAria2Resource) Delete(ctx context.Context, req resource.DeleteRequest, resp *resource.DeleteResponse) {
+	var client *DownloadClientAria2
+
+	resp.Diagnostics.Append(req.State.Get(ctx, &client)...)
+
+	if resp.Diagnostics.HasError() {
+		return
+	}
+
+	// Delete DownloadClientAria2 current value
+	err := r.client.DeleteDownloadClientContext(ctx, client.ID.ValueInt64())
+	if err != nil {
+		resp.Diagnostics.AddError(tools.ClientError, fmt.Sprintf("Unable to read %s, got error: %s", downloadClientAria2ResourceName, err))
+
+		return
+	}
+
+	tflog.Trace(ctx, "deleted "+downloadClientAria2ResourceName+": "+strconv.Itoa(int(client.ID.ValueInt64())))
+	resp.State.RemoveResource(ctx)
+}
+
+func (r *DownloadClientAria2Resource) ImportState(ctx context.Context, req resource.ImportStateRequest, resp *resource.ImportStateResponse) {
+	// resource.ImportStatePassthroughID(ctx, path.Root("id"), req, resp)
+	id, err := strconv.Atoi(req.ID)
+	if err != nil {
+		resp.Diagnostics.AddError(
+			tools.UnexpectedImportIdentifier,
+			fmt.Sprintf("Expected import identifier with format: ID. Got: %q", req.ID),
+		)
+
+		return
+	}
+
+	tflog.Trace(ctx, "imported "+downloadClientAria2ResourceName+": "+strconv.Itoa(id))
+	resp.Diagnostics.Append(resp.State.SetAttribute(ctx, path.Root("id"), id)...)
+}
+
+func (d *DownloadClientAria2) write(ctx context.Context, downloadClient *sonarr.DownloadClientOutput) {
+	genericDownloadClient := DownloadClient{
+		Enable:                   types.BoolValue(downloadClient.Enable),
+		RemoveCompletedDownloads: types.BoolValue(downloadClient.RemoveCompletedDownloads),
+		RemoveFailedDownloads:    types.BoolValue(downloadClient.RemoveFailedDownloads),
+		Priority:                 types.Int64Value(int64(downloadClient.Priority)),
+		ID:                       types.Int64Value(downloadClient.ID),
+		Name:                     types.StringValue(downloadClient.Name),
+		Tags:                     types.SetValueMust(types.Int64Type, nil),
+	}
+	tfsdk.ValueFrom(ctx, downloadClient.Tags, genericDownloadClient.Tags.Type(ctx), &genericDownloadClient.Tags)
+	genericDownloadClient.writeFields(ctx, downloadClient.Fields)
+	d.fromDownloadClient(&genericDownloadClient)
+}
+
+func (d *DownloadClientAria2) read(ctx context.Context) *sonarr.DownloadClientInput {
+	var tags []int
+
+	tfsdk.ValueAs(ctx, d.Tags, &tags)
+
+	return &sonarr.DownloadClientInput{
+		Enable:                   d.Enable.ValueBool(),
+		RemoveCompletedDownloads: d.RemoveCompletedDownloads.ValueBool(),
+		RemoveFailedDownloads:    d.RemoveFailedDownloads.ValueBool(),
+		Priority:                 int(d.Priority.ValueInt64()),
+		ID:                       d.ID.ValueInt64(),
+		ConfigContract:           DownloadClientAria2ConfigContrat,
+		Implementation:           DownloadClientAria2Implementation,
+		Name:                     d.Name.ValueString(),
+		Protocol:                 DownloadClientAria2Protocol,
+		Tags:                     tags,
+		Fields:                   d.toDownloadClient().readFields(ctx),
+	}
+}
--- internal/provider/provider.go
+++ internal/provider/provider.go
@@ -135,6 +135,7 @@ func (p *SonarrProvider) Resources(ctx context.Context) []func() resource.Resour
 		NewDelayProfileResource,
 		NewDownloadClientConfigResource,
 		NewDownloadClientResource,
+		NewDownloadClientAria2Resource,
 		NewDownloadClientDelugeResource,
 		NewDownloadClientTorrentDownloadStationResource,
 		NewDownloadClientFloodResource,
