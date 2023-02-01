8837f2b891a1514fe87888e9d26d9fd3edb9d0fe
Fuochi <ffuochi@hotmail.com>
2023-02-01 14:33:17 +0400

    test: fix quality profile custom format conflict

--- internal/provider/quality_profile_data_source_test.go
+++ internal/provider/quality_profile_data_source_test.go
@@ -18,41 +18,14 @@ func TestAccQualityProfileDataSource(t *testing.T) {
 				Config: testAccQualityProfileDataSourceConfig,
 				Check: resource.ComposeAggregateTestCheckFunc(
 					resource.TestCheckResourceAttrSet("data.sonarr_quality_profile.test", "id"),
-					resource.TestCheckResourceAttr("data.sonarr_quality_profile.test", "cutoff", "1100")),
+					resource.TestCheckResourceAttr("data.sonarr_quality_profile.test", "cutoff", "1")),
 			},
 		},
 	})
 }
 
 const testAccQualityProfileDataSourceConfig = `
-resource "sonarr_quality_profile" "test" {
-	name            = "qpdata"
-	upgrade_allowed = true
-	cutoff          = 1100
-
-	quality_groups = [
-		{
-			id   = 1100
-			name = "4k"
-			qualities = [
-				{
-					id         = 18
-					name       = "WEBDL-2160p"
-					source     = "web"
-					resolution = 2160
-				},
-				{
-					id         = 19
-					name       = "Bluray-2160p"
-					source     = "bluray"
-					resolution = 2160
-				}
-			]
-		}
-	]
-}
-
 data "sonarr_quality_profile" "test" {
-	name = sonarr_quality_profile.test.name
+	name = "Any"
 }
 `
--- internal/provider/quality_profile_resource_test.go
+++ internal/provider/quality_profile_resource_test.go
@@ -8,8 +8,7 @@ import (
 )
 
 func TestAccQualityProfileResource(t *testing.T) {
-	t.Parallel()
-
+	// no parallel to avoid conflict with custom formats
 	resource.Test(t, resource.TestCase{
 		PreCheck:                 func() { testAccPreCheck(t) },
 		ProtoV6ProviderFactories: testAccProtoV6ProviderFactories,
