fbf9141873f28af03b6f0dc46b51cff26982975b
Fuochi <ffuochi@hotmail.com>
2023-02-03 10:09:43 +0400

    rework: improve field code reuse

--- internal/helpers/fields.go
+++ internal/helpers/fields.go
@@ -16,6 +16,7 @@ type fieldException struct {
 	tfName  string
 }
 
+// getFieldExceptions identifies the fields resources in which TF and API name differs.
 func getFieldExceptions() []fieldException {
 	return []fieldException{
 		{
@@ -37,6 +38,7 @@ func getFieldExceptions() []fieldException {
 	}
 }
 
+// selectTFName identifies the TF name starting from API name.
 func selectTFName(name string) string {
 	for _, f := range getFieldExceptions() {
 		if f.apiName == name {
@@ -47,6 +49,7 @@ func selectTFName(name string) string {
 	return name
 }
 
+// selectAPIName identifies the API name starting from TF name.
 func selectAPIName(name string) string {
 	for _, f := range getFieldExceptions() {
 		if f.tfName == name {
@@ -57,180 +60,149 @@ func selectAPIName(name string) string {
 	return name
 }
 
+// selectWriteField identifies which struct field should be written.
+func selectWriteField(fieldOutput *sonarr.Field, fieldCase interface{}) reflect.Value {
+	fieldName := selectTFName(fieldOutput.GetName())
+	value := reflect.ValueOf(fieldCase).Elem()
+
+	return value.FieldByNameFunc(func(n string) bool { return strings.EqualFold(n, fieldName) })
+}
+
+// selectReadField identifies which struct field should be read.
+func selectReadField(name string, fieldCase interface{}) reflect.Value {
+	value := reflect.ValueOf(fieldCase)
+	value = value.Elem()
+	return value.FieldByNameFunc(func(n string) bool { return strings.EqualFold(n, name) })
+}
+
+// setField sets the sonarr field value.
+func setField(name string, value interface{}) *sonarr.Field {
+	field := sonarr.NewField()
+	field.SetName(name)
+	field.SetValue(value)
+
+	return field
+}
+
+// WriteStringField writes a sonarr string field into struct field.
 func WriteStringField(fieldOutput *sonarr.Field, fieldCase interface{}) {
-	fieldName := selectTFName(fieldOutput.GetName())
 	stringValue := fmt.Sprint(fieldOutput.GetValue())
-	value := reflect.ValueOf(fieldCase)
-	value = value.Elem()
-	field := value.FieldByNameFunc(func(n string) bool { return strings.EqualFold(n, fieldName) })
 	v := reflect.ValueOf(types.StringValue(stringValue))
-	field.Set(v)
+	selectWriteField(fieldOutput, fieldCase).Set(v)
 }
 
+// WriteBoolField writes a sonarr bool field into struct field.
 func WriteBoolField(fieldOutput *sonarr.Field, fieldCase interface{}) {
-	fieldName := selectTFName(fieldOutput.GetName())
 	boolValue, _ := fieldOutput.GetValue().(bool)
-	value := reflect.ValueOf(fieldCase)
-	value = value.Elem()
-	field := value.FieldByNameFunc(func(n string) bool { return strings.EqualFold(n, fieldName) })
 	v := reflect.ValueOf(types.BoolValue(boolValue))
-	field.Set(v)
+	selectWriteField(fieldOutput, fieldCase).Set(v)
 }
 
+// WriteIntField writes a sonarr int field into struct field.
 func WriteIntField(fieldOutput *sonarr.Field, fieldCase interface{}) {
-	fieldName := selectTFName(fieldOutput.GetName())
 	intValue, _ := fieldOutput.GetValue().(float64)
-	value := reflect.ValueOf(fieldCase)
-	value = value.Elem()
-	field := value.FieldByNameFunc(func(n string) bool { return strings.EqualFold(n, fieldName) })
 	v := reflect.ValueOf(types.Int64Value(int64(intValue)))
-	field.Set(v)
+	selectWriteField(fieldOutput, fieldCase).Set(v)
 }
 
+// WriteFloatField writes a sonarr float field into struct field.
 func WriteFloatField(fieldOutput *sonarr.Field, fieldCase interface{}) {
-	fieldName := selectTFName(fieldOutput.GetName())
 	floatValue, _ := fieldOutput.GetValue().(float64)
-	value := reflect.ValueOf(fieldCase)
-	value = value.Elem()
-	field := value.FieldByNameFunc(func(n string) bool { return strings.EqualFold(n, fieldName) })
 	v := reflect.ValueOf(types.Float64Value(floatValue))
-	field.Set(v)
+	selectWriteField(fieldOutput, fieldCase).Set(v)
 }
 
+// WriteStringSliceField writes a sonarr string slice field into struct field.
 func WriteStringSliceField(ctx context.Context, fieldOutput *sonarr.Field, fieldCase interface{}) {
-	fieldName := selectTFName(fieldOutput.GetName())
 	sliceValue, _ := fieldOutput.GetValue().([]interface{})
-	value := reflect.ValueOf(fieldCase)
-	value = value.Elem()
 	setValue := types.SetValueMust(types.StringType, nil)
 	tfsdk.ValueFrom(ctx, sliceValue, setValue.Type(ctx), &setValue)
-
-	field := value.FieldByNameFunc(func(n string) bool { return strings.EqualFold(n, fieldName) })
 	v := reflect.ValueOf(setValue)
-	field.Set(v)
+	selectWriteField(fieldOutput, fieldCase).Set(v)
 }
 
+// WriteIntSliceField writes a sonarr int slice field into struct field.
 func WriteIntSliceField(ctx context.Context, fieldOutput *sonarr.Field, fieldCase interface{}) {
 	sliceValue, _ := fieldOutput.GetValue().([]interface{})
-	value := reflect.ValueOf(fieldCase)
-	value = value.Elem()
 	setValue := types.SetValueMust(types.Int64Type, nil)
 	tfsdk.ValueFrom(ctx, sliceValue, setValue.Type(ctx), &setValue)
-
-	field := value.FieldByNameFunc(func(n string) bool { return strings.EqualFold(n, fieldOutput.GetName()) })
 	v := reflect.ValueOf(setValue)
-	field.Set(v)
+	selectWriteField(fieldOutput, fieldCase).Set(v)
 }
 
+// ReadStringField reads from a string struct field and return a sonarr field.
 func ReadStringField(name string, fieldCase interface{}) *sonarr.Field {
 	fieldName := selectAPIName(name)
-	value := reflect.ValueOf(fieldCase)
-	value = value.Elem()
-	field := value.FieldByNameFunc(func(n string) bool { return strings.EqualFold(n, name) })
-	stringField := (*types.String)(field.Addr().UnsafePointer())
+	stringField := (*types.String)(selectReadField(name, fieldCase).Addr().UnsafePointer())
 
 	if !stringField.IsNull() && !stringField.IsUnknown() {
-		field := sonarr.NewField()
-		field.SetName(fieldName)
-		field.SetValue(stringField.ValueString())
-
-		return field
+		return setField(fieldName, stringField.ValueString())
 	}
 
 	return nil
 }
 
+// ReadBoolField reads from a bool struct field and return a sonarr field.
 func ReadBoolField(name string, fieldCase interface{}) *sonarr.Field {
 	fieldName := selectAPIName(name)
-	value := reflect.ValueOf(fieldCase)
-	value = value.Elem()
-	field := value.FieldByNameFunc(func(n string) bool { return strings.EqualFold(n, name) })
-	boolField := (*types.Bool)(field.Addr().UnsafePointer())
+	boolField := (*types.Bool)(selectReadField(name, fieldCase).Addr().UnsafePointer())
 
 	if !boolField.IsNull() && !boolField.IsUnknown() {
-		field := sonarr.NewField()
-		field.SetName(fieldName)
-		field.SetValue(boolField.ValueBool())
-
-		return field
+		return setField(fieldName, boolField.ValueBool())
 	}
 
 	return nil
 }
 
+// ReadIntField reads from a int struct field and return a sonarr field.
 func ReadIntField(name string, fieldCase interface{}) *sonarr.Field {
 	fieldName := selectAPIName(name)
-	value := reflect.ValueOf(fieldCase)
-	value = value.Elem()
-	field := value.FieldByNameFunc(func(n string) bool { return strings.EqualFold(n, name) })
-	intField := (*types.Int64)(field.Addr().UnsafePointer())
+	intField := (*types.Int64)(selectReadField(name, fieldCase).Addr().UnsafePointer())
 
 	if !intField.IsNull() && !intField.IsUnknown() {
-		field := sonarr.NewField()
-		field.SetName(fieldName)
-		field.SetValue(intField.ValueInt64())
-
-		return field
+		return setField(fieldName, intField.ValueInt64())
 	}
 
 	return nil
 }
 
+// ReadFloatField reads from a float struct field and return a sonarr field.
 func ReadFloatField(name string, fieldCase interface{}) *sonarr.Field {
 	fieldName := selectAPIName(name)
-	value := reflect.ValueOf(fieldCase)
-	value = value.Elem()
-	field := value.FieldByNameFunc(func(n string) bool { return strings.EqualFold(n, name) })
-	floatField := (*types.Float64)(field.Addr().UnsafePointer())
+	floatField := (*types.Float64)(selectReadField(name, fieldCase).Addr().UnsafePointer())
 
 	if !floatField.IsNull() && !floatField.IsUnknown() {
-		field := sonarr.NewField()
-		field.SetName(fieldName)
-		field.SetValue(floatField.ValueFloat64())
-
-		return field
+		return setField(fieldName, floatField.ValueFloat64())
 	}
 
 	return nil
 }
 
+// ReadStringSliceField reads from a string slice struct field and return a sonarr field.
 func ReadStringSliceField(ctx context.Context, name string, fieldCase interface{}) *sonarr.Field {
 	fieldName := selectAPIName(name)
-	value := reflect.ValueOf(fieldCase)
-	value = value.Elem()
-	field := value.FieldByNameFunc(func(n string) bool { return strings.EqualFold(n, name) })
-	sliceField := (*types.Set)(field.Addr().UnsafePointer())
+	sliceField := (*types.Set)(selectReadField(name, fieldCase).Addr().UnsafePointer())
 
 	if len(sliceField.Elements()) != 0 {
 		slice := make([]string, len(sliceField.Elements()))
 		tfsdk.ValueAs(ctx, sliceField, &slice)
 
-		field := sonarr.NewField()
-		field.SetName(fieldName)
-		field.SetValue(slice)
-
-		return field
+		return setField(fieldName, slice)
 	}
 
 	return nil
 }
 
+// ReadIntSliceField reads from a int slice struct field and return a sonarr field.
 func ReadIntSliceField(ctx context.Context, name string, fieldCase interface{}) *sonarr.Field {
 	fieldName := selectAPIName(name)
-	value := reflect.ValueOf(fieldCase)
-	value = value.Elem()
-	field := value.FieldByNameFunc(func(n string) bool { return strings.EqualFold(n, name) })
-	sliceField := (*types.Set)(field.Addr().UnsafePointer())
+	sliceField := (*types.Set)(selectReadField(name, fieldCase).Addr().UnsafePointer())
 
 	if len(sliceField.Elements()) != 0 {
 		slice := make([]int64, len(sliceField.Elements()))
 		tfsdk.ValueAs(ctx, sliceField, &slice)
 
-		field := sonarr.NewField()
-		field.SetName(fieldName)
-		field.SetValue(slice)
-
-		return field
+		return setField(fieldName, slice)
 	}
 
 	return nil
