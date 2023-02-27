7d685bce962d024896de5b5e2e3eaa9260febdbd
Fuochi <ffuochi@hotmail.com>
2023-02-27 14:06:44 +0400

    feat: add auto tags data source

---
+++ examples/data-sources/sonarr_auto_tags/data-source.tf
@@ -0,0 +1,2 @@
+data "sonarr_auto_tags" "example" {
+}
---
+++ internal/provider/auto_tags_data_source_test.go
@@ -0,0 +1,40 @@
+package provider
+
+import (
+	"regexp"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+)
+
+func TestAccAutoTagsDataSource(t *testing.T) {
+	t.Parallel()
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:                 func() { testAccPreCheck(t) },
+		ProtoV6ProviderFactories: testAccProtoV6ProviderFactories,
+		Steps: []resource.TestStep{
+			// Unauthorized
+			{
+				Config:      testAccAutoTagsDataSourceConfig + testUnauthorizedProvider,
+				ExpectError: regexp.MustCompile("Client Error"),
+			},
+			// Create a delay profile to have a value to check
+			{
+				Config: testAccTagResourceConfig("test", "dataautotag") + testAccAutoTagResourceConfig("datasourceTest", "true"),
+			},
+			// Read testing
+			{
+				Config: testAccAutoTagsDataSourceConfig,
+				Check: resource.ComposeAggregateTestCheckFunc(
+					resource.TestCheckTypeSetElemNestedAttrs("data.sonarr_auto_tags.test", "auto_tags.*", map[string]string{"remove_tags_automatically": "true"}),
+				),
+			},
+		},
+	})
+}
+
+const testAccAutoTagsDataSourceConfig = `
+data "sonarr_auto_tags" "test" {
+}
+`
