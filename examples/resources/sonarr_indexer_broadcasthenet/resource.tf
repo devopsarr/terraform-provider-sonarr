a06047f08c59022be6799648c9f80a5beb7daf75
Fuochi <ffuochi@hotmail.com>
2023-05-11 12:27:10 +0400

    doc: fix indexer broadcasthenet example

--- examples/resources/sonarr_indexer_broadcasthenet/resource.tf
+++ examples/resources/sonarr_indexer_broadcasthenet/resource.tf
@@ -1,4 +1,4 @@
-resource "sonarr_indexer_rargb" "example" {
+resource "sonarr_indexer_broadcasthenet" "example" {
   enable_automatic_search = true
   name                    = "Example"
   base_url                = "https://api.broadcasthe.net/"
